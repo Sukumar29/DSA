1b35390d2f0f36f307cb1dd7c18e14aea2bccfd1
Sukumar Reddy <ksukumar2908@gmail.com>
2026-08-09 14:52:48 +0530

    Time: 0 ms (100.00%) | Memory: 80.5 MB (22.92%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -13,11 +13,11 @@ class Solution {
             else if(nums[mid]>nums[mid-1]){
                 low=mid+1;
             }
-            else if(nums[mid]>nums[mid+1]){
-                high=mid-1;
-            }
+            // else if(nums[mid]>nums[mid+1]){
+            //     high=mid-1;
+            // }
             else{
-                low=mid+1;
+                high=mid-1;
             }
         }
         return -1;
