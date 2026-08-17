4389294895141534910c6ceb218e1450d8b8355b
Sukumar Reddy <ksukumar2908@gmail.com>
2026-08-17 17:44:14 +0530

    Time: 5 ms (73.75%) | Memory: 43.7 MB (15.49%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,14 +1,23 @@
 class Solution {
     public int hIndex(int[] citations) {
+        // Arrays.sort(citations);
+        // int n=citations.length;
+        // int h=0;
+        // for(int i=0;i<n;i++){
+        //     int papers=n-i;
+        //     if(citations[i]>=papers){
+        //         h++;
+        //     }
+        // }
+        // return h;
         Arrays.sort(citations);
-        int n=citations.length;
-        int h=0;
-        for(int i=0;i<n;i++){
-            int papers=n-i;
-            if(citations[i]>=papers){
-                h++;
-            }
+        int h_index = 0;
+        for (int i = citations.length - 1; i >= 0; i--) {
+            if (citations[i] > h_index)
+                h_index++;
+            else
+                break;
         }
-        return h;
+        return h_index;
     }
 }
